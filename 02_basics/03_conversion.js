95229777058d41f73916b4b8ca52941c1cd6268a
SatyamD1301 <166933927+SatyamD1301@users.noreply.github.com>
2024-04-14 04:26:41 +0000

    conversion_of_data_types

---
+++ 02_basics/03_conversion.js
@@ -0,0 +1,46 @@
+// let score="33"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let value=Number(score)
+// console.log(typeof value)
+
+/*
+ans is 
+string
+string
+number
+*/
+// let score="33abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let value=Number(score)
+// console.log(typeof value)
+
+/*
+ans is 
+string
+string
+number
+*/
+
+// let score=null
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let value=Number(score)
+// console.log(typeof value)
+
+/*
+object 
+object
+number
+*/
+// if we convert into boolean it change into true or false
+
+
+//we can convert into string and many more
