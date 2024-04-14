2d736081938633b989d3c36dda3e737447e756db
SatyamD1301 <166933927+SatyamD1301@users.noreply.github.com>
2024-04-14 03:36:28 +0000

    datatypes

---
+++ 02_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="satya@satya.com"
+var accountPassword="12345"
+accountCity="Gorakhpur"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+/*
+prefer not to use var because it scope is not defined
+
+=>If variable decale but not defined then javascript default it undefined
+
+
+
+
+*/
+
---
+++ 02_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+// "use strict "//=>is use to for newer version of javascript
+
+// alert("hello") => is used for only browser not in nodejs
+
+// we must have to make our code readable
+
+// avascript automaticatilly removed spaces
+
+let name="Satyam" //=>string ""
+let age=18  //=>integer
+let coder=true  //=>booolean true/false
+
+// null =>standalone value
+
+//undefined=> not defined
+
+//symbol =>uniquness
+
+//object 
+console.log(typeof "satyam")
+console.log(typeof 18)
+
+console.log(typeof null)
+console.log(typeof undefined)
+// console.table(typeof["satyam",18,null,undefined,])
+
