3d2e2dd626d361d7ed4aba3db926ed0a15a20a74
SatyamD1301 <166933927+SatyamD1301@users.noreply.github.com>
2024-09-22 08:36:48 +0000

    date

---
+++ 02_basics/07_date.js
@@ -0,0 +1,28 @@
+// Date
+
+let myDate=new Date()
+// console.log(myDate);  //2024-09-22T05:50:06.763Z
+// console.log(myDate.toString()); //Sun Sep 22 2024 05:50:06 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); //Sun Sep 22 2024
+// console.log(myDate.toISOString()); //2024-09-22T05:50:06.763Z
+// console.log(myDate.toLocaleString()); //9/22/2024, 5:50:06 AM
+// console.log(myDate.toTimeString()); //05:50:06 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toLocaleTimeString()); //5:50:06 AM
+
+console.log(typeof(myDate));
+
+// let myCreatedDate=new Date(2024,12,22)
+// let myCreatedDate=new Date(2024,12,0)
+
+// let myCreatedDate=new Date("2023-01-14")
+let myCreatedDate=new Date("1-03-2023")
+
+// the date in javascript when predefined 
+// the month is start from 0 the date is start from 1and more if we write 0 it roll back to the 
+
+// the month of 12 if we write it roll back to the next year jan 2025
+
+// console.log(myCreatedDate.toDateString());
+
+
+
